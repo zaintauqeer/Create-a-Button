40c8ac43cf3d1b7af520820d54e81575c7634123
Shehorz Saleem <shehrozsaleem616@gmail.com>
2024-11-28 10:30:47 +0500

    remove google

--- src/app/(dashboard)/logo.tsx
+++ src/app/(dashboard)/logo.tsx
@@ -14,7 +14,7 @@ export const Logo = () => {
     <Link href="/">
       <div className="flex items-center gap-x-2 hover:opacity-75 transition h-[68px] px-4">
         <div className="size-8 relative">
-          <Image src="/icon.jpeg" alt="Image AI" fill />
+          <Image src="/icon.jpg" alt="Image AI" fill />
         </div>
         <h1 className={cn(font.className, "text-xl font-bold")}>
           Image AI
