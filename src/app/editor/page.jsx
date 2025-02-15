ce14b64d6e74fb777fdd027d07aabaec39e4a9a5
Shehorz Saleem <shehrozsaleem616@gmail.com>
2025-02-15 19:45:23 +0500

    edit

---
+++ src/app/editor/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Editor } from "@/features/editor/components/editor";
+
+export default function EditorPage() {
+    const data = {
+        "id": "",
+        "name": "Test",
+        "json":"",
+        "height": 1200,
+        "width": 900,
+        "thumbnailUrl": null,
+        "isTemplate": null,
+        "isPro": null,
+        "createdAt": "2024-11-26T04:58:53.031Z",
+        "updatedAt": "2024-11-27T05:25:05.526Z"
+    }
+  return (
+    <Editor initialData={data}/>
+  )
+}
